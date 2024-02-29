e74e8b40a3fc0e834b5767ae9d74f29c14d5e2c1
Himanshu V Chaurasia <himanshuchaurasia015@gmail.com>
2024-03-01 02:08:33 +0530

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,4 +1,4 @@
-"use strict";
+
 const nodeMailer = require("nodemailer");
 
 const sendEmail = async(options)=>{
@@ -13,17 +13,7 @@ const sendEmail = async(options)=>{
         }
     });
 
-    // let transporter = nodemailer.createTransport({
-    //     service: 'gmail',
-    //     auth: {
-    //       type: 'OAuth2',
-    //       user: process.env.SMTP_MAIL ,
-    //       pass: process.env.SMTP_PASSWORD,
-    //       clientId: "415645094136-t7kj489njab1lmfld8siqo0tan69e1ck.apps.googleusercontent.com",
-    //       clientSecret:  "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",
-    //       refreshToken:  "1//04vDgj17dMCycCgYIARAAGAQSNwF-L9IrRIWCaa5zlbrLakaQr79bb8u5evhWtPkCrvZUz8unTWQxPlemI-VluRWNgWhZIlxu8bI"
-    //     }
-    //   });
+  
  
     const mailOptions ={
         from: process.env.SMTP_MAIL,
