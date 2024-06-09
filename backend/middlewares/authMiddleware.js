092904994c13ab51fdaac6561d1db95637361174
Himanshu V Chaurasia <himanshuchaurasia015@gmail.com>
2024-06-09 10:25:48 +0530

    User Controllers Updated

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,22 +1,28 @@
-import asyncHandler from "./asyncHandler";
+import asyncHandler from "./asyncHandler.js";
+import jwt from 'jsonwebtoken';
+import User from "../models/userModel.js";
 
 const authenticate= asyncHandler(async(req,res,next)=>{
+
     let token;
 
-    token=req.cookie.jwt;
+    token=req.cookies.jwt;
+    
 
     if (token){
         try {
 
-            const decoded = jwt.verify(token,process.env.JWT_SECRET);
-            req.user=await User.findById(decoded.userId).select("-password");
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            req.user = await User.findById(decoded.userId).select("-password");
             next();
 
             
         } catch (error) {
 
             res.status(401);
-            throw new Error("Not Authorized");
+            console.log(error)
+            throw new Error("Not Authorized, Token failed");
             
         }
     }else{
@@ -27,3 +33,14 @@ const authenticate= asyncHandler(async(req,res,next)=>{
 
 
 });
+
+
+const authorizeAdmin=(req,res,next)=>{
+    if(req.user && req.user.isAdmin){
+        next()
+    }else{
+        res.status(401).send("Not Authorized Admin")
+    }
+}
+
+export {authenticate,authorizeAdmin};
---
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import {AiOutlineHome, AiOutlineShopping, AiOutlineLogin, AiOutlineUserAdd, AiOutlineShoppingCart} from 'react-icons/ai'
+
+
+
+const Navigation = () => {
+  return (
+    <div>
+      <h1>Navigation</h1>
+    </div>
+  )
+}
+
+export default Navigation
