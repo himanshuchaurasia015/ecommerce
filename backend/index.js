befea5fb69cfb98eca7ff885eb0134fc62b1dd62
Himanshu V Chaurasia <himanshuchaurasia015@gmail.com>
2024-06-11 14:31:31 +0530

    product  routes done

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,9 @@ import cookieParser from 'cookie-parser'
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+// import uploadRoutes from "./routes/uploadRoutes.js";
+// import orderRoutes from "./routes/orderRoutes.js";
 
 dotenv.config();
 const port =process.env.PORT||5000;
@@ -21,7 +24,7 @@ app.use(cookieParser())
 
 app.use('/api/users', userRoutes);
 app.use("/api/category", categoryRoutes);
-// app.use("/api/products", productRoutes);
+app.use("/api/products", productRoutes);
 // app.use("/api/upload", uploadRoutes);
 // app.use("/api/orders", orderRoutes);
 
