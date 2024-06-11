e705de07dda5f973f686a8835e906a9dd9df0c53
Himanshu V Chaurasia <himanshuchaurasia015@gmail.com>
2024-06-11 17:42:46 +0530

    upload route added

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
-// import uploadRoutes from "./routes/uploadRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 // import orderRoutes from "./routes/orderRoutes.js";
 
 dotenv.config();
@@ -25,8 +25,12 @@ app.use(cookieParser())
 app.use('/api/users', userRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
-// app.use("/api/upload", uploadRoutes);
+app.use("/api/upload", uploadRoutes);
 // app.use("/api/orders", orderRoutes);
+
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
 app.listen(port, ()=>{
     console.log(`Server is running on ${port}`)
